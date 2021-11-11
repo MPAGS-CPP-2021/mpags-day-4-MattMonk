95642b921c0e39c69f801df46ca40a2e4258f2f7
Matthew Monk <monk.matthew@gmail.com>
2021-11-11 10:31:48 +0000

    Added unit test for playfair cipher

---
+++ src/Testing/testPlayfairCipher.cpp
@@ -0,0 +1,17 @@
+//! Unit Tests for MPAGSCipher PlayfairCipher Class
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+
+#include "PlayfairCipher.hpp"
+
+TEST_CASE("Caesar Cipher encryption", "[caesar]")
+{
+    PlayfairCipher cc{"playfair"};
+    REQUIRE(cc.applyCipher("HELLOWORLD", CipherMode::Encrypt) == "KGYVRVVQGRMF");
+}
+
+TEST_CASE("Caesar Cipher decryption", "[caesar]")
+{
+    PlayfairCipher cc{"playfair"};
+    REQUIRE(cc.applyCipher("KGYVRVVQGRMF", CipherMode::Decrypt) == "HELLOWORLD");
+}
